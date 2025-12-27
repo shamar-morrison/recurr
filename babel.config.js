a87b5cae16299feb8df72520ca1fca3d5245e53f
Shamar Morrison <shamar.webdev@gmail.com>
2025-12-27 18:52:03 -0500

    feat: Add EAS build configuration, `expo-dev-client` dependency, and production console removal.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
 
+  const plugins = [];
+
+  // Remove console statements in production
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push([
+      'transform-remove-console',
+      {
+        // exclude: ['error', 'warn'],
+      },
+    ]);
+  }
+
   return {
     presets: [['babel-preset-expo'], 'nativewind/babel'],
 
     plugins: [
+      ...plugins,
       [
         'module-resolver',
         {
